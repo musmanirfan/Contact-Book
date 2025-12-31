dcd731dda1cd28518dca9c1bf0ddc7dcdb221597
musmanirfan <usmanmala356@gmail.com>
2025-12-31 22:57:39 +0500

    contact-book v-1.0

---
+++ ContactsAPI/Migrations/20221226171051_Data tpe change for the property 'Phone'.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactsAPI.Migrations
+{
+    public partial class DatatpechangeforthepropertyPhone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Phone",
+                table: "Contacts",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
